41c2ec99e419ac60cd8c31e53160f2432ec2f4d7
ElizaZhou96 <131576351+ElizaZhou96@users.noreply.github.com>
2026-04-18 12:35:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -121,7 +121,7 @@ function App() {
             </p>
 
             <div className="mt-10 w-full relative overflow-hidden rounded-2xl">
-              <div className="w-full h-[220px] md:h-[280px] overflow-hidden">
+              <div className="w-full h-[240px] md:h-[300px] overflow-hidden">
                 <img
                   src="/ijcai_banner.png"
                   alt="AIM4DK banner"
