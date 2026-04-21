d4e6a33955dbd9a18467b3c5fb9b5dd11ca0f9cc
ElizaZhou96 <131576351+ElizaZhou96@users.noreply.github.com>
2026-04-21 12:12:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
-import Sponsorship from './Sponsorship.tsx';
+import App from './App';
+import Sponsorship from './Sponsorship';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
