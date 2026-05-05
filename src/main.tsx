1f76a63d28bea28f27031bb0490b8680e8f55f61
ElizaZhou96 <131576351+ElizaZhou96@users.noreply.github.com>
2026-05-05 08:48:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Sponsorship from './Sponsorship';
+// import Sponsorship from './Sponsorship';
 import './index.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sponsorship" element={<Sponsorship />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
+
+// createRoot(document.getElementById('root') as HTMLElement).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/sponsorship" element={<Sponsorship />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
